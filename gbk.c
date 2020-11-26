7196ef8eaa0b34032bdd9ba6122e1c6575abceb6
Anthony Baah <abaah17@alustudent.com>
2020-11-26 23:25:08 +0200

    Implemented building blocks

---
+++ gbk.c
@@ -0,0 +1,179 @@
+#include "gbk.h"
+
+static void handlerc(int);
+void cmdmv(cmdnode **, int);
+void xcmd(char **, int, alias **);
+/**
+ *main - main loop for the gbk shell
+ *@argc: number of arguments
+ *@argv: script file names
+ *@argp: envrionmental variables
+ *Return: returns the index of 0 on sucess
+ */
+int main(int argc, char **argv, char **argp)
+{
+	char *cmd = NULL, **cmds = NULL;
+	size_t cmdlen = 0;
+	int mode = !isatty(0), i = 0, cmdnum = 1, inputs = 0;
+	alias *head = NULL;
+
+	/*initialize environmental variables*/
+	environ = sarrdup(argp);
+	if (argc > 1)
+	{
+		runscript(argv[1]);
+		exit(0);
+	}
+	/*initialize erro function*/
+	perr(argv[0], &inputs, NULL);
+	/*initialize signal handler*/
+	signal(SIGINT, handlerc);
+	while (1)
+	{
+		/*get command and parse based on ;*/
+		cmd = NULL, cmds = NULL, getinput(&cmd, &cmdlen, &cmds);
+		cmdnum = arlen(cmds), i = 0;
+		free(cmd);
+		inputs++;/*increase the number of commands accepted*/
+		/*execute all the commands one by one*/
+		while (i < cmdnum)
+			xcmd(cmds, i, &head), i++;
+		if (cmdnum > 0)
+			freedp(cmds);
+		if (mode)
+			exit(255);
+	}
+	free(environ);
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ *handlerc - handlce SIGNUM
+ *@signum: sig ID
+ */
+static void handlerc(int signum)
+{
+	write(1, "\n$ ", 3);
+	(void)signum;
+}
+
+/**
+ *cmdmv - moves the command list to the next command
+ *accorindign to the commands and the execute statues
+ *of the current cmd
+ *@head:a pointer to a pointer to the current commands
+ *@childstat: the status of the execution of the current command
+ *node
+ */
+void cmdmv(cmdnode **head, int childstat)
+{
+	cmdnode *tmp = NULL;
+
+	if (!_strcmp((*head)->op, "||"))
+	{
+		if (!childstat)
+		{
+			free((*head)->op);
+			(*head)->op = (*head)->next->op;
+
+			tmp = (*head)->next->next;
+
+			free((*head)->next->cmd);
+			free((*head)->next);
+			(*head)->next = tmp;
+			(*head)->estat = 1;
+		}
+		else
+			(*head) = (*head)->next;
+
+	}
+	else if (!_strcmp((*head)->op, "&&"))
+	{
+		if (!childstat)
+			(*head)->estat = 1, (*head) = (*head)->next;
+		else
+		{
+			free((*head)->op);
+			(*head)->op = (*head)->next->op;
+			tmp = (*head)->next->next;
+
+			free((*head)->next->cmd);
+			free((*head)->next);
+			(*head)->next = tmp;
+		}
+	}
+	else
+		(*head) = (*head)->next;
+}
+/**
+ *xcmd - parses, and uses the excute function
+ *to creats a process and excute the cmd
+ *@cmd_l: commands list
+ *@index: current command index
+ *@aliashead: head of the laias list
+ *@prog: name of the program
+ *@rcmds: number of commands that has already run
+ */
+void xcmd(char **cmd_l, int index, alias **aliashead)
+{
+	char **tmp = NULL, *cmds = cmd_l[index];
+	int childid, *binstat, exitstat;
+	static int childstat;
+	cmdnode *head = NULL, *_head = NULL;
+
+	_head = build_list(cmds), head = _head;
+	while (head)
+	{
+		strexpand(&(head->cmd), childstat);
+		parseargs(head->cmd, " ", &tmp, 0);
+		whistory(cmds);
+		binstat = handlebin(tmp, aliashead);
+		if (!binstat[0])
+		{
+			if (binstat[1] != 266)
+			{
+				exitstat = binstat[1];
+				freedp(environ), free(binstat), freedp(tmp);
+				freedp(cmd_l), free_cmdlist(_head);
+				exit(exitstat);
+			}
+			cmdmv(&head, 0), free(binstat);
+			continue;
+		}
+		free(binstat);
+		if (head->estat == 0)
+		{
+			fflush(stdout);
+			childid = fork();
+			if (childid < 0)
+			{
+				perror("ERROR: coudn't creat a child proccess");
+				exit(-1);
+			}
+			if (childid == 0)
+			{
+				childstat = execute(tmp);
+				freedp(environ), freedp(tmp), freedp(cmd_l), free_cmdlist(_head);
+				exit(childstat);
+			}
+			else
+			{
+				wait(&childstat);
+				cmdmv(&head, childstat);
+			}
+		}
+		else
+		{
+			if (!_strcmp(head->op, "||"))
+			{
+				free(head->op);
+				head->op = head->next->op;
+				head->next = head->next->next;
+			}
+			else
+				head = head->next;
+		}
+		freedp(tmp);
+	}
+	free_cmdlist(_head);
+}
---
+++ gbk_utils.c
@@ -0,0 +1,109 @@
+#include "gbk.h"
+
+/**
+ *getfpath - gets the full path if possible of name from
+ *the environment variable
+ *@name: the name of comand
+ *Return: a full path of name or name it self on failure
+ */
+char *getfpath(char *name)
+{
+	char *PATH = _getenv("PATH"), *fpath = NULL;
+	char *tokenized = _strtok(PATH, ":", 0);
+	struct stat fileinfo;
+
+	if (!name)
+		return (NULL);
+	/*incase it is alreay a full path*/
+	/*
+	 *3. handle commands like /bin, that are like paths but dont do anythin
+	 */
+	if (*name == '/' || *name == '.')
+	{
+		fpath = smalloc(_strlen(name) * sizeof(char) + 1);
+		_strcpy(fpath, name);
+		return (fpath);
+	}
+	/*incase it is found in one of the path locations*/
+	while (tokenized)
+	{
+		fpath = smalloc((_strlen(tokenized) + _strlen(name) + 1) * sizeof(char) + 1);
+		_strcpy(fpath, tokenized);
+		_strcat(fpath, "/");
+		_strcat(fpath, name);
+		if (!stat(fpath, &fileinfo))
+			return (fpath);
+		free(fpath);
+		tokenized = _strtok(NULL, ":", 0);
+	}
+	/*incase it couldnt be found any where*/
+	fpath = smalloc(_strlen(name) * sizeof(char) + 1);
+	_strcpy(fpath, name);
+	return (fpath);
+}
+
+/**
+ *parseargs - returns an array of arguments from a string
+ *@cmd: the command string to tokenize
+ *@del: delimeter to parse the arguments with
+ *@args: a pointer to an array for the the arguments
+ *@mod: delimeter method 0-character 1-string
+ */
+void parseargs(char *cmd, const char *del, char ***args, int mod)
+{
+	char *tokenized = NULL, **tmp;
+	int index = 0, i;
+
+	*args = NULL;
+	tokenized = _strtok(cmd, del, mod);
+	while (tokenized != NULL)
+	{
+		/*1 for the new string, 1 for the NULL*/
+		tmp = (char **)smalloc((arlen(*args) +  2) * sizeof(char *));
+		if (!*args)
+			*args = tmp;
+		else
+		{
+			i = 0;
+			while ((*args)[i])
+			{
+				tmp[i] = _strdup((*args)[i]);
+				i++;
+			}
+			freedp(*args);
+			*args = tmp;
+		}
+		(*args)[index] = NULL;
+		/*trim tokenized and store it in *args[index]*/
+		trims(&((*args)[index]), tokenized);
+		(*args)[index + 1] = NULL;
+		tokenized = _strtok(NULL, del, mod);
+		index++;
+	}
+}
+
+/**
+ *getinput - gets input from the command line and parses it in
+ *to sub commands if any
+ *@input: pointer to the input string
+ *@inputlen: pointer to the len of input
+ *@cmds: pointer to the array of the parsed input
+ *Return: lenght of input
+ */
+int getinput(char **input, size_t *inputlen, char ***cmds)
+{
+	int len;
+
+	if (isatty(0))
+		write(1, "$ ", 2);
+	len = _getline(input, inputlen, 0);
+	if (len == -1)
+	{
+		free(*input);
+		exit(-1);
+	}
+	/*replace the ending new line with \0*/
+	(*input)[len - 1] = '\0';
+	parseargs(*input, ";", cmds, 0);
+	return (len - 1);
+}
