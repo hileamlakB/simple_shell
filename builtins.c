6b1a30c44dabc9bc4faacc63dc056875c0bf60c5
hileamlak <hilea.buisness@gmail.com>
2020-11-30 05:09:42 +0000

    Improve error handling and exit handling

--- builtins.c
+++ builtins.c
@@ -1,6 +1,47 @@
 #include "gbk.h"
+#define l(x) _strlen((x))
 
+/**
+*handle_exit - decides exit code
+*@cmd: list of args
+*Return: exit code
+*/
+int handle_exit(char **cmd)
+{
+	int exitstatus = 0;
 
+	if (cmd[1])
+	{
+		while (cmd[1][i])
+		{
+			if (!('0' <= cmd[1][i] && '9' >= cmd[1][i]))
+				str_in = 1;
+			i++;
+		}
+		ermsg = smalloc(l("exit: Illegal number: ") + l(cmd[1]) + 4);
+		_strcpy(ermsg, "exit: Illegal number: ");
+		_strcat(ermsg, cmd[1]);
+
+		if (str_in)
+		{
+			errno = -1;
+			perr(NULL, NULL, ermsg);
+			exitstatus = 2;
+
+		}
+		else if (_atoi(cmd[1]) < 0)
+		{
+			errno = -1;
+			perr(NULL, NULL, ermsg);
+			exitstatus = 2;
+		}
+		else
+			exitstatus = _atoi(cmd[1]) % 256;
+
+		free(ermsg);
+	}
+	return (exitstatus);
+}
 /**
  *handlebin - handles builtin commands
  *@cmd: command arguements
@@ -10,15 +51,14 @@
 int *handlebin(char **cmd, alias **head)
 {
 	int exitstatus = 0;
-	int *ret = smalloc(2 * sizeof(int));
+	int *ret = smalloc(2 * sizeof(int)), i = 0, str_in = 0;
+	char *ermsg;
 
 	ret[0] = 1, ret[1] = 266;
+	if (!cmd)
+		return (ret);
 	if (!_strcmp(cmd[0], "exit"))
-	{
-		if (cmd[1])
-			exitstatus = _atoi(cmd[1]) % 256;
-		ret[0] = 0, ret[1] = exitstatus;
-	}
+		ret[0] = 0, ret[1] = handle_exit(cmd);
 	else if (!_strcmp(cmd[0], "env") || !_strcmp(cmd[0], "printenv"))
 		_printenv(), ret[0] = 0;
 	else if (!_strcmp(cmd[0], "\n"))
@@ -26,14 +66,18 @@ int *handlebin(char **cmd, alias **head)
 	else if (!_strcmp(cmd[0], "setenv"))
 	{
 		if (arlen(cmd) != 3)
-			perr(NULL, NULL, "Too few or too many arguements");
-		_setenv(cmd[1], cmd[2], 0), ret[0] = 0;
+			errno = -2, perr(NULL, NULL, "Too few or too many arguements");
+		else
+			_setenv(cmd[1], cmd[2], 0);
+		ret[0] = 0;
 	}
 	else if (!_strcmp(cmd[0], "unsetenv"))
 	{
 		if (arlen(cmd) != 2)
-			perr(NULL, NULL, "Too few or too many arguements");
-		_unsetenv(cmd[1]), ret[0] = 0;
+			errno = -2, perr(NULL, NULL, "Too few or too many arguements");
+		else
+			_unsetenv(cmd[1]);
+		ret[0] = 0;
 	}
 	else if (!_strcmp(cmd[0], "cd"))
 		_chdir(arlen(cmd) > 1 ? cmd[1] : NULL),	ret[0] = 0;
@@ -43,7 +87,6 @@ int *handlebin(char **cmd, alias **head)
 		phelp(arlen(cmd) > 1 ? cmd[1] : NULL),	ret[0] = 0;
 	else if (!_strcmp(cmd[0], "alias"))
 		handle_alias(cmd, head), ret[0] = 0;
-
 	if (!ret[0] && ret[1] == 266)
 		freedp(cmd);
 	return (ret);
