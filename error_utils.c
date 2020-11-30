6506c365de963436f6f13f80f09f7af636ad76c7
hileamlak <hilea.buisness@gmail.com>
2020-11-30 05:11:33 +0000

    Handle negatie errors with custom messagess

--- error_utils.c
+++ error_utils.c
@@ -6,7 +6,7 @@
  *@cmdr: pointer to number of commands run including this one
  *@msg: message to be printed
  */
-void perr(char *prog, int *cmdr, __attribute__((unused))char *msg)
+void perr(char *prog, int *cmdr, char *msg)
 {
 	char *numcmd;
 	static int *cmdsrun;
@@ -27,10 +27,11 @@ void perr(char *prog, int *cmdr, __attribute__((unused))char *msg)
 		_write(2, numcmd, _strlen(numcmd));
 		free(numcmd);
 		_write(2, ": ", 2);
-		_write(2, (char *)errlist[errno], _strlen((char *)errlist[errno]));
+		if (errno < 0)
+			_write(2, msg, _strlen(msg));
+		else
+			_write(2, (char *)errlist[errno], _strlen((char *)errlist[errno]));
 		_write(2, "\n", 1);
 		_write(2, NULL, 0);
-
-
 	}
 }
